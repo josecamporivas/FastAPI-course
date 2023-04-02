25cf68e450308a955bc7b8a23bdebe43017fd955
Jose Campo Rivas <josecampor31@gmail.com>
2023-04-02 14:51:56 +0200

    add static files

--- routermain.py
+++ routermain.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from routers import products
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Routers
 app.include_router(products.router)
+
+# Static files
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 
 @app.get("/")
